9c65559a443e5f2bd3c1b5c782a0a129ce7bd1f1
Mihai Tomescu <matomesc@gmail.com>
2023-06-29 14:47:54 -0700

    feat: switch back to useNavigate hook as it's working now

--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -1,24 +1,13 @@
-import { useAppBridge } from '@shopify/app-bridge-react';
+import { useNavigate } from '@shopify/app-bridge-react';
 import { GetServerSideProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { Redirect as AppBridgeRedirect } from '@shopify/app-bridge/actions';
 
 const Redirect: NextPage = () => {
   const router = useRouter();
-  const app = useAppBridge();
-
-  // Currently this hook is broken. It doesn't redirect properly as it
-  // duplicates the /store/somestore path. Only seems to be broken for Shopify
-  // urls (eg. billing confirmation url).
-  // See:
-  // https://github.com/Shopify/shopify-app-bridge/issues/214
-  // https://github.com/Shopify/shopify-app-bridge/issues/213
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const redirectUrl = decodeURIComponent(String(router.query.redirectUrl));
-  // navigate(redirectUrl);
-  const redirect = AppBridgeRedirect.create(app);
-  redirect.dispatch(AppBridgeRedirect.Action.REMOTE, redirectUrl);
+  navigate(redirectUrl);
 
   return <div />;
 };
