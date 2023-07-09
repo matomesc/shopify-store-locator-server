c6c6fcc306d06848868dfa299ea63db46ad3cd5e
Mihai Tomescu <matomesc@gmail.com>
2023-07-08 17:14:49 -0700

    feat: check if router is ready before accessing query params

--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -1,13 +1,18 @@
 import { useNavigate } from '@shopify/app-bridge-react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const Redirect: NextPage = () => {
   const router = useRouter();
   const navigate = useNavigate();
 
-  const redirectUrl = decodeURIComponent(String(router.query.redirectUrl));
-  navigate(redirectUrl);
+  useEffect(() => {
+    if (router.isReady) {
+      const redirectUrl = decodeURIComponent(String(router.query.redirectUrl));
+      navigate(redirectUrl);
+    }
+  }, [navigate, router.isReady, router.query.redirectUrl]);
 
   return <div />;
 };
