f1368bea20c70ee413ff1a38ef830ae8bf2e31b3
Mihai Tomescu <matomesc@gmail.com>
2024-06-18 21:03:49 +0000

    feat: remove navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,12 @@ import type { AppProps, AppType } from 'next/app';
 import { AppProvider as PolarisAppProvider } from '@shopify/polaris';
 import enTranslations from '@shopify/polaris/locales/en.json';
 import { LinkWrapper } from '@/client/components/LinkWrapper';
-import { NavBar } from '@/client/components/NavBar';
 import { trpc } from '@/lib/trpc';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const App: AppType = ({ Component, pageProps }: AppProps) => {
   return (
     <PolarisAppProvider i18n={enTranslations} linkComponent={LinkWrapper}>
-      <NavBar />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} />
