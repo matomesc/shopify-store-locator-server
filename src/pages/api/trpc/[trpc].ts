ae59b272c7d640978f02399c56a0f3978fbfd35a
Mihai Tomescu <matomesc@gmail.com>
2024-09-01 00:35:46 +0000

    feat: add trpc api route body parser limit

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -3,6 +3,14 @@ import * as Sentry from '@sentry/nextjs';
 import { createContext } from '../../../server/trpc/context';
 import { appRouter } from '../../../server/trpc/routers/_app';
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '15mb',
+    },
+  },
+};
+
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
