8d9a20876b8e2680f654a0f308502445d90ae035
Mihai Tomescu <matomesc@gmail.com>
2024-09-01 20:59:26 +0000

    fix: fix session prune worker connection

--- src/server/workers/SessionPruneWorker.ts
+++ src/server/workers/SessionPruneWorker.ts
@@ -4,7 +4,7 @@ import { chunk } from 'lodash';
 import { DateTime } from 'luxon';
 import { logger } from '../lib/logger';
 import { BaseWorker } from './BaseWorker';
-import { clearRepeatableJobs, queues } from '../lib/bullmq';
+import { clearRepeatableJobs, queues, workerRedisClient } from '../lib/bullmq';
 import { prisma } from '../lib/prisma';
 
 export class SessionPruneWorker extends BaseWorker {
@@ -34,44 +34,48 @@ export class SessionPruneWorker extends BaseWorker {
       removeOnFail: true,
     });
 
-    this.bullmqWorker = new Worker(queues.SessionPrune.name, async () => {
-      try {
-        const shops = await prisma.shop.findMany({
-          include: {
-            plan: true,
-          },
-        });
-        const chunks = chunk(shops, 5);
-
-        // eslint-disable-next-line no-restricted-syntax
-        for (const shopsChunk of chunks) {
-          const promises = shopsChunk.map(async (shop) => {
-            const { analyticsRetention } = shop.plan;
-
-            await prisma.session.deleteMany({
-              where: {
-                shopId: shop.id,
-                createdAt: {
-                  lt: DateTime.utc()
-                    .minus({ days: analyticsRetention })
-                    .toJSDate(),
-                },
-              },
-            });
+    this.bullmqWorker = new Worker(
+      queues.SessionPrune.name,
+      async () => {
+        try {
+          const shops = await prisma.shop.findMany({
+            include: {
+              plan: true,
+            },
           });
+          const chunks = chunk(shops, 5);
 
-          // eslint-disable-next-line no-await-in-loop
-          await Promise.all(promises);
+          // eslint-disable-next-line no-restricted-syntax
+          for (const shopsChunk of chunks) {
+            const promises = shopsChunk.map(async (shop) => {
+              const { analyticsRetention } = shop.plan;
+
+              await prisma.session.deleteMany({
+                where: {
+                  shopId: shop.id,
+                  createdAt: {
+                    lt: DateTime.utc()
+                      .minus({ days: analyticsRetention })
+                      .toJSDate(),
+                  },
+                },
+              });
+            });
+
+            // eslint-disable-next-line no-await-in-loop
+            await Promise.all(promises);
+          }
+
+          this.logger.info(`Pruned sessions for ${shops.length} shops`);
+        } catch (err) {
+          // Log the error
+          Sentry.captureException(err);
+
+          // Rethrow the error to fail the job
+          throw err;
         }
-
-        this.logger.info(`Pruned sessions for ${shops.length} shops`);
-      } catch (err) {
-        // Log the error
-        Sentry.captureException(err);
-
-        // Rethrow the error to fail the job
-        throw err;
-      }
-    });
+      },
+      { connection: workerRedisClient, concurrency: 1 },
+    );
   }
 }
